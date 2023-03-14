a5df8fa448fa4043a8b1825766b4cd41eadb0614
TheodosisKats <theodosis.kats@gmail.com>
2023-03-14 02:54:48 +0200

    Added features to app logic & Updated Readme

--- Program.cs
+++ Program.cs
@@ -4,9 +4,8 @@ using Microsoft.Graph.Models;
 
 var scopes = new[] { "User.Read" };
 
-var tenantId = "cf6cda30-1730-43c0-925d-8c5aebfdf535";
-var clientId = "cefafc21-5154-41ed-9b55-5b7080a226fc";
-var ordersTodoList = "AAMkAGIwODg4NWMyLTk5YTgtNGE5Zi1iMjdjLTdhMDNmNzAyMjllMwAuAAAAAADCZnWuYygKQp1EKwyGLDx4AQA6tPYtcRxXSKIPcU6ONsCnAAJjE4_5AAA=";
+const string tenantId = "cf6cda30-1730-43c0-925d-8c5aebfdf535";
+const string clientId = "cefafc21-5154-41ed-9b55-5b7080a226fc";
 var interactiveCredential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
 {
     TenantId = tenantId,
@@ -17,38 +16,126 @@ var interactiveCredential = new InteractiveBrowserCredential(new InteractiveBrow
 
 var graphClient = new GraphServiceClient(interactiveCredential, scopes);
 
-//TODO : get todo lists
+var todoListCollection = await graphClient.Me.Todo.Lists.GetAsync();
 
-//find if Orders list exists
-    //if exists get id
-    //else 
-        //create orders list
-        //get task lists
-        //get orders list id
+//find or create the Orders & Stock TodoLists
+var ordersTodoList = await FindOrCreateOrdersList(todoListCollection);
+var stockTodoList = await FindOrCreateStockList(todoListCollection);
 
-// // Create Orders Todo list
-// var requestBody = new TodoTaskList { DisplayName = $"Orders" };
-// await graphClient.Me.Todo.Lists.PostAsync(requestBody);
+var newOrderInfo = GenerateNewOrder();
 
+await AddOrderToMyLists(ordersTodoList.Id, stockTodoList.Id, newOrderInfo);
 
-var products = new List<string> { "Product A", "Product B", "Product C", "Product D", "Product E" };
-string randomProduct = products[new Random().Next(products.Count)];
-int orderNumber = new Random().Next(10000, 99999);
-bool inStock = new Random().Next(2) != 0;
-DateTime currentDate = DateTime.Now;
-DateTime shippingDate = inStock ? currentDate.AddDays(3) : currentDate.AddDays(7);
-
-var requestBody = new TodoTask
+async Task<TodoTaskList> FindOrCreateOrdersList(TodoTaskListCollectionResponse todoListCollection)
 {
-	Title = $"Order: {orderNumber}",
-	Body = new ItemBody { Content = $"Product: {randomProduct} \n shippingDate: {shippingDate.ToShortDateString()} \n isInStock: {inStock.ToString()}" },
-	Categories = new List<string>
-	{
-		"Orders",
-	},
-};
+    if (todoListCollection.Value.Count > 0)
+    {
+        foreach (var todoList in todoListCollection.Value)
+        {
+            if (todoList.DisplayName == "Orders")
+            {
+                return todoList;
+            }
+        }
+    }
+    
+    // If the "Orders" list does not exist, create it
+    var requestBody = new TodoTaskList
+    {
+        DisplayName = "Orders"
+    };
+    var createdTodoList = await graphClient.Me.Todo.Lists.PostAsync(requestBody);
+    return createdTodoList;
+}
 
-requestBody.Body.Content = $"Product: {randomProduct} \n shippingDate: {shippingDate.ToShortDateString()} \n isInStock: {inStock.ToString()}";
+async Task<TodoTaskList> FindOrCreateStockList(TodoTaskListCollectionResponse todoListCollection)
+{
+    if (todoListCollection.Value.Count > 0)
+    {
+        foreach (var todoList in todoListCollection.Value)
+        {
+            if (todoList.DisplayName == "Stock")
+            {
+                return todoList;
+            }
+        }
+    }
+    // If the "Stock" list does not exist, create it
+    var requestBody = new TodoTaskList
+    {
+        DisplayName = "Stock"
+    };
+    var createdTodoList = await graphClient.Me.Todo.Lists.PostAsync(requestBody);
+    return createdTodoList;
+}
 
-await graphClient.Me.Todo.Lists[$"{ordersTodoList}"].Tasks.PostAsync(requestBody);
+Order GenerateNewOrder()
+{
+    var products = new List<string> { "Product A", "Product B", "Product C", "Product D", "Product E" };
+    string randomProduct = products[new Random().Next(products.Count)];
+    int orderNumber = new Random().Next(001, 100);
+    int quantity = new Random().Next(1, 10);
+    bool inStock = new Random().Next(2) != 0;;
+    DateTime currentDate = DateTime.Now;
+    DateTime shippingDate;
+    
+    if (inStock)
+    {
+        shippingDate = currentDate.AddDays(3);
+    }
+    else
+    {
+        shippingDate = currentDate.AddDays(7);
+    }
 
+    // Check if the shipping date is on a Saturday or Sunday
+    if (shippingDate.DayOfWeek == DayOfWeek.Saturday)
+    {
+        shippingDate = shippingDate.AddDays(2);
+    }
+    else if (shippingDate.DayOfWeek == DayOfWeek.Sunday)
+    {
+        shippingDate = shippingDate.AddDays(1);
+    }
+    
+    var newOrder = new TodoTask
+    {
+        Title = $"Order: {orderNumber}",
+        Body = new ItemBody { Content = $"Product: {randomProduct} \n Shipping Date: {shippingDate.ToShortDateString()} \n is In Stock: {inStock.ToString()}" },
+        Categories = new List<string>
+        {
+            "Orders",
+        },
+        DueDateTime = shippingDate.ToDateTimeTimeZone()
+    };
+
+    //If product out of stock, create task to restock
+    TodoTask stockInfo = new TodoTask();
+    if(inStock == false)
+    {
+        stockInfo = new TodoTask
+        {
+            Title = $"Restock {randomProduct} | Quantity: {quantity} | Order: {orderNumber}",
+            Body = new ItemBody { Content = $"Product: {randomProduct} \n Stock needed: {quantity}" },
+            Categories = new List<string>
+            {
+                "Stock",
+            },
+            DueDateTime = shippingDate.ToDateTimeTimeZone()
+        };
+    }
+
+    return new Order { OrderInfo = newOrder, StockInfo = stockInfo };
+}
+
+async Task AddOrderToMyLists(string ordersListId,string stockListId, Order newOrderInfo)
+{
+    await graphClient.Me.Todo.Lists[$"{ordersListId}"].Tasks.PostAsync(newOrderInfo.OrderInfo);
+    await graphClient.Me.Todo.Lists[$"{stockListId}"].Tasks.PostAsync(newOrderInfo.StockInfo);
+}
+
+public class Order
+{
+    public TodoTask OrderInfo { get; set; }
+    public TodoTask StockInfo { get; set; }
+}
--- README.md
+++ README.md
@@ -7,7 +7,9 @@ This code is a sample implementation of using Azure Graph API to manage tasks in
 The main purpose of this code is to create and manage tasks in a Todo List. In this example, the code creates a Todo List named "Orders" if it doesn't exist and then adds a new task with random order details to the "Orders" list.
 
 ## Prerequisites
-- Azure subscription with access to Graph API
+- An Azure account
+- A registered application in Azure
+- The client ID and tenant ID for the registered application
 - Microsoft Graph .NET SDK (version 1.0)
 - Azure.Identity package (version 1.8)
 
@@ -16,7 +18,12 @@ The main purpose of this code is to create and manage tasks in a Todo List. In t
 - Open the solution in Visual Studio and update the `tenantId` and `clientId` variables with your Azure AD tenant and client IDs respectively.
 - Run the code.
 
-The code will open a browser window prompting the user to sign in with their Azure AD credentials. Once authenticated, the code will access the Graph API and create a new Todo List named "Orders" (if it doesn't exist), and add a new task to the list with random order details.
+## Code Explanation
+- The FindOrCreateOrdersList() and FindOrCreateStockList() methods check if the Orders and Stock lists already exist. If they do, the method returns the TodoTaskList. If they do not exist, the method creates the TodoTaskList and returns it.
+- The GenerateNewOrder() method generates a new order with a random product, order number, quantity, and shipping date and randomly picks if the ordered product is in stock or not. If the product is not in stock, the method also generates a new task for restocking.
+- If the generated shipping date falls on weekend then it is rescheduled for the following Mondays.
+- The AddOrderToMyLists() method adds the new order and the restocking task (if applicable) to the Orders and Stock TodoLists using the PostAsync() method on the Tasks endpoint.
+- The code will open a browser window prompting the user to sign in with their Azure AD credentials. Once authenticated, the code will access the Graph API and create a new Todo List named "Orders" (if it doesn't exist), and add a new task to the list with the random generated order details and will create a Stock task if the item ordered is not in stock.
 
 ## Note
 This code is a sample implementation and should not be used in production environments without appropriate modifications and testing. Additionally, it assumes that the user has appropriate permissions to create and manage Todo Lists using the Graph API.
